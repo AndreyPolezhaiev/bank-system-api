dd3af2b1b63ae7ab3adde6082ba1e9b1224a78ba
Andrey Polezhaiev <workpolezhaiev@gmail.com>
2024-08-28 20:23:21 +0300

    add user entity

---
+++ src/main/java/com/polezhaiev/banksystemapi/model/User.java
@@ -0,0 +1,36 @@
+package com.polezhaiev.banksystemapi.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Data;
+
+@Entity
+@Data
+@Table(name = "users")
+public class User {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false, name = "first_name")
+    private String firstName;
+
+    @Column(nullable = false, name = "last_name")
+    private String lastName;
+
+    @Column(nullable = false, name = "phone_number", unique = true)
+    private String phoneNumber;
+
+    @Column(nullable = false, unique = true)
+    private String email;
+
+    @Column(nullable = false)
+    private String password;
+
+    public User() {
+    }
+}
